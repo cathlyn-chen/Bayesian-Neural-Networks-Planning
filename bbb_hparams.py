b74a0bff47f7b926dce22b2bf43b24aedaa0164d
Cathlyn <cathlynchen18@gmail.com>
2021-06-07 16:06:20 -0400

    Initialized hparams for mnist

--- bbb_hparams.py
+++ bbb_hparams.py
@@ -2,23 +2,24 @@ import math
 import torch
 
 
-def hparams():
+class Hparams:
+    def __init__(self) -> None:
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
 
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    LOADER_KWARGS = {
-        'num_workers': 1,
-        'pin_memory': True
-    } if torch.cuda.is_available() else {}
-    print(torch.cuda.is_available())
 
-    BATCH_SIZE = 1000
-    TEST_BATCH_SIZE = 5
+def mnist():
+    hparams = Hparams()
+    hparams.batch_size = 1000
+    hparams.test_batch_size = 5
 
-    CLASSES = 10
-    TRAIN_EPOCHS = 20
-    SAMPLES = 2
-    TEST_SAMPLES = 10
+    hparams.classes = 10
+    hparams.trian_epochs = 20
+    hparams.samples = 2
+    hparams.test_samples = 10
 
-    PI = 0.5
-    SIGMA_1 = torch.FloatTensor([math.exp(-0)])
-    SIGMA_2 = torch.FloatTensor([math.exp(-6)])
+    hparams.pi = 0.5
+    hparams.sigma_1 = torch.FloatTensor([math.exp(-0)])
+    hparams.sigma_2 = torch.FloatTensor([math.exp(-6)])
+
+    return hparams
